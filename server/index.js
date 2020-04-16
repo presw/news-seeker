814ba9cf3c1009c163ac972270fcb2dc43311f8c
Preston Whitehall <pmwhitehall@gmail.com>
2020-04-16 01:42:28 -0700

    Adds cors to try to fix cross origin errors

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '/../dist')));
+app.use(cors());
 
 app.listen(PORT, () => console.log(`App listening on port: ${PORT}`));
